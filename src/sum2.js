dc46e1fa8ea4dd81c9374512e3dac66df9ab0640
Raul Ortega <raul.ortega@autodesk.com>
2023-04-14 10:13:20 -0600

    almost

---
+++ src/sum2.js
@@ -0,0 +1,31 @@
+import {stop, start, sayst} from '../support/benchmark.js';
+// Declare function
+//function say(something) {
+
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+const twoSum = (nums, target) => {
+//var twoSum = function(nums, target) {
+    start();
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                stop();
+                return [i, j]
+            }
+        }
+    }
+
+};
+
+// Call twoSum function
+sayst(twoSum([2,7,11,15], 9));
+
+//export module
+if (typeof module === 'object') {
+    module.exports = twoSum;
+}
---
+++ support/benchmark.js
@@ -0,0 +1,29 @@
+var startTime, endTime;
+
+export const start = () => {
+    console.log('start');
+
+  startTime = performance.now();
+};
+
+export const stop = () => {
+  console.log('stop');
+  endTime = performance.now();
+  var timeDiff = endTime - startTime; //in ms
+  // strip the ms
+  //timeDiff /= 1000;
+
+  // get ms 
+  //var ms = Math.round(timeDiff);
+  console.log(timeDiff + " ms");
+};
+
+export const sayst = (something) => {
+  
+    if (typeof window !== 'undefined') {
+      //console.log(alert === window.alert); // 👉️ true    
+      alert(something);// 👉️ can use alert()
+    } else {
+      console.log(something);    // 👉️ can't use alert()
+    }
+  };
